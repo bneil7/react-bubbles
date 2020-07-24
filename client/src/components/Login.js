9789eaba77d5d56f9ac02b23047c93346d4db528
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-07-24 10:28:20 -0600

    axios request, auth, and private routing works. had to revert the username and password on the server back to the original because i kept getting 403.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const { push } = useHistory();
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", formData)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        push("/colors");
+      })
+      .catch(err => console.log("WHOOPS... ", err));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
