d76885c5e70c7b03b13f713e06a99b21114de746
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-07-24 12:39:36 -0600

    finished step 2, bubble page and color list automatically rerender on edit and delete. project meets MVP now.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
 const ColorList = ({ colors, updateColors }) => {
@@ -20,11 +20,32 @@ const ColorList = ({ colors, updateColors }) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
+    // where is is saved right now? ????????
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log("PUT", res.data);
+        axiosWithAuth()
+          .get("http://localhost:5000/api/colors/")
+          .then(response => {
+            updateColors(response.data);
+          });
+      })
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log("DELETE", res);
+        axiosWithAuth()
+          .get("http://localhost:5000/api/colors/")
+          .then(response => {
+            updateColors(response.data);
+          });
+      });
   };
 
   return (
@@ -34,12 +55,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -68,7 +91,7 @@ const ColorList = ({ colors, updateColors }) => {
               onChange={e =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value }
+                  code: { hex: e.target.value },
                 })
               }
               value={colorToEdit.code.hex}
